71d0f8d09bb0de47b1137a741e0f7e4a2ff1ad04
anilbolat <anilbolat90@gmail.com>
2024-12-24 12:22:07 +0200

    login and register works

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,39 @@
-function Login(props) {
+import { useState } from "react";
 
-    function handleFormSubmit(event) {
+function Login(props) {
+    const [formData, setFormData] = useState({ "email": "", "password": "" });
+
+    const handleInputChange = (event) => {
         event.preventDefault();
-        console.log(event);
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        login(formData.email, formData.password);
+    }
+
+    const login = (email, password) => {
+        fetch("/api/v1/auth/login",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ "email": email, "password": password })
+
+            })
+            .then((response) => response.json(), (response) => console.log("login req is rejected: " + response))
+            .catch((reason) => console.log("reason: " + reason));
     }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <form onSubmit={handleFormSubmit}>
                 <div style={{ textAlign: 'center', margin: '10px' }}>
-                    <input className="input-group-text" type="text" name="username" placeholder="Username" />
+                    <input className="input-group-text" type="text" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange} />
                 </div>
                 <div style={{ textAlign: 'center', margin: '10px' }}>
-                    <input className="input-group-text" type="password" name="password" placeholder="Password" />
+                    <input className="input-group-text" type="password" name="password" placeholder="Password" value={formData.password} onChange={handleInputChange} />
                 </div>
                 <div style={{ textAlign: 'center', margin: '30px' }}>
                     <button type="submit" className="btn btn-info">Login</button>
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,18 +1,41 @@
-function Register(props) {
+import { useState } from "react";
 
-    function handleFormSubmit(event) {
+function Register(props) {
+    const [formData, setFormData] = useState({ "email": "", "password": "" });
+
+    const handleInputChange = (event) => {
         event.preventDefault();
-        console.log(event);
+        const { name, value } = event.target;
+        
+        console.log(formData);
+        setFormData({ ...formData, [name]: value });
+        console.log(formData);
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        register(formData.email, formData.password);
+    }
+
+    const register = (email, password) => {
+        fetch("/api/v1/auth/register",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ "email": email, "password": password })
+            })
+            .then((response) => response.json(), (response) => console.log("register req is rejected: " + response))
+            .catch((reason) => console.log("reason: " + reason));
     }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <form onSubmit={handleFormSubmit}>
                 <div style={{ textAlign: 'center', margin: '10px' }}>
-                    <input className="input-group-text" type="text" name="username" placeholder="Username" />
+                    <input className="input-group-text" type="text" name="email" placeholder="Email" value={formData.email} onChange={handleInputChange} />
                 </div>
                 <div style={{ textAlign: 'center', margin: '10px' }}>
-                    <input className="input-group-text" type="password" name="password" placeholder="Password" />
+                    <input className="input-group-text" type="password" name="password" placeholder="Password" value={formData.password} onChange={handleInputChange} />
                 </div>
                 <div style={{ textAlign: 'center', margin: '30px' }}>
                     <button type="submit" className="btn btn-info">Sign up</button>
